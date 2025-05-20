89c726718b23b1c49a87a0f2dc1adae7dcd8ac8e
Avinash Kumar <avi.k.s.4653@gmail.com>
2025-05-20 16:32:26 +0530

    mplemented relationship between entities in database

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -36,10 +36,14 @@ const taskSchema = new mongoose.Schema({
   },
   comments: [
     {
-      text: String,
+      text: {
+        type: String,
+        required: true,
+      },
       user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
+        required: true,
       },
       createdAt: {
         type: Date,
@@ -54,6 +58,42 @@ const taskSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Add indexes for better query performance
+taskSchema.index({ project: 1 });
+taskSchema.index({ assignedTo: 1 });
+taskSchema.index({ createdBy: 1 });
+taskSchema.index({ status: 1 });
+
+// Middleware to update related documents when a task is created
+taskSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    try {
+      const Project = mongoose.model("Project");
+      const User = mongoose.model("User");
+
+      // Add task to project's tasks array
+      await Project.findByIdAndUpdate(this.project, {
+        $push: { tasks: this._id },
+      });
+
+      // Add task to creator's createdTasks array
+      await User.findByIdAndUpdate(this.createdBy, {
+        $push: { createdTasks: this._id },
+      });
+
+      // If task is assigned, add to assignee's assignedTasks array
+      if (this.assignedTo) {
+        await User.findByIdAndUpdate(this.assignedTo, {
+          $push: { assignedTasks: this._id },
+        });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+  next();
 });
 
 module.exports = mongoose.model("Task", taskSchema);
